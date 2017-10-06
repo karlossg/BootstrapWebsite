6b07b9175100585de3aa4caeb30631cbfb273c9c
Karol <karol.gratus@gmail.com>
2017-10-06 08:28:03 +0200

    added tooltips and popups

---
+++ script.js
@@ -0,0 +1,12 @@
+$(function () {
+  $('[data-toggle="popover"]').popover()
+});
+
+$(document).on('click', function (e) {
+    $('[data-toggle="popover"],[data-original-title]').each(function () {
+        if (!$(this).is(e.target) && $(this).has(e.target).length === 0 && $('.popover').has(e.target).length === 0) {                
+            (($(this).popover('hide').data('bs.popover')||{}).inState||{}).click = false  
+        }
+
+    });
+});
