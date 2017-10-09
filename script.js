3eeccea2a5e1978428fd473fda81bf1f5e22019d
Karol <karol.gratus@gmail.com>
2017-10-09 11:05:35 +0200

    tootlip fixed

--- script.js
+++ script.js
@@ -2,6 +2,10 @@ $(function () {
   $('[data-toggle="popover"]').popover()
 });
 
+$(document).ready(function(){
+    $('[data-toggle="tooltip"]').tooltip(); 
+});
+
 $(document).on('click', function (e) {
     $('[data-toggle="popover"],[data-original-title]').each(function () {
         if (!$(this).is(e.target) && $(this).has(e.target).length === 0 && $('.popover').has(e.target).length === 0) {                
